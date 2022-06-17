fcabbfd03657453932132770d0b98ec7f7356f4b
INA <codechemi@gmail.com>
2022-06-18 03:05:24 +0900

    Refactor : Display Air Quality.

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ def SelectWeather(LocalCode):
 
     # 온도
     Temp = soup.select('.cmp-cur-weather > ul.wrap-1 > li > span.tmp')
-    TempContent = "🌡 :  " + Temp[0].contents[0] + "℃"
+    TempContent = " 🌡 :  " + Temp[0].contents[0] + "℃"
     print(TempContent)
 
     # 습도
