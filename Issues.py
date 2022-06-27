b7bba3553883937634d8ca8be2536ade754d469e
INA <codechemi@gmail.com>
2022-06-27 15:02:48 +0900

    Add Debug Code.

--- Issues.py
+++ Issues.py
@@ -25,9 +25,17 @@ def update_gist(access_token, gist_id, title, content):
 
 
 if __name__ == "__main__":
+    # Debug Acces Token
+    # access_token = ""
     access_token = os.environ['MY_GITHUB_TOKEN']
+    # Debug Gist id
+    # gist_id = ""
     gist_id = os.environ['GIST_ID']
+    # Debug Gist Name
+    # gist_name = "제주 🍊, 푸른 바당 🌊"
     gist_name = os.environ['GIST_NAME']
+    # Debug Reg Code
+    # reg_code = "5011059000"
     reg_code = os.environ['REG_CODE']
 
     repository_name = "WhereWeather"
@@ -35,13 +43,10 @@ if __name__ == "__main__":
     today = datetime.now(seoul_timezone)
     today_date = today.strftime("%Y년 %m월 %d일 %H시 %M분")
 
-    # issue_title = "[" + today_date + "] 제주 🍊, 푸른 바당 🌊"
     issue_title = "[" + today_date + "] " + gist_name
-    # upload_contents = SelectWeather("5011059000")
     upload_contents = SelectWeather(reg_code)
     repo = get_github_repo(access_token, repository_name)
 
-    # update_gist(access_token, gist_id, "제주 🍊, 푸른 바당 🌊", upload_contents)
     update_gist(access_token, gist_id, gist_name, upload_contents)
     print("Update Github Gist Success!")
 
