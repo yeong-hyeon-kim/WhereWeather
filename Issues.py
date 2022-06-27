eb13bf50c746c85b7e5fde4f7a03bf899b8bd60b
INA <codechemi@gmail.com>
2022-06-27 13:07:07 +0900

    Change : Issue Title.

--- Issues.py
+++ Issues.py
@@ -32,11 +32,11 @@ if __name__ == "__main__":
     today = datetime.now(seoul_timezone)
     today_date = today.strftime("%Y년 %m월 %d일 %H시 %M분")
 
-    issue_title = "[" + today_date + "] 제주, 푸른 바당 🌊"
+    issue_title = "[" + today_date + "] 제주 🍊, 푸른 바당 🌊"
     upload_contents = SelectWeather("5011063000")
     repo = get_github_repo(access_token, repository_name)
 
-    update_gist(access_token, gist_id, "제주, 푸른 바당 🌊", upload_contents)
+    update_gist(access_token, gist_id, "제주 🍊, 푸른 바당 🌊", upload_contents)
     print("Update Github Gist Success!")
 
     upload_github_issue(repo, issue_title, upload_contents)
